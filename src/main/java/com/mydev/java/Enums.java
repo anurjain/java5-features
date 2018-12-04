4a0328fa9a24a56f194e2abb16b206706338a007
CORP\anurjain <anurag.jain@capgemini.com>
2018-12-04 23:22:37 +0100

    Enum

--- src/main/java/com/mydev/java/Enums.java
+++ src/main/java/com/mydev/java/Enums.java
@@ -1,11 +1,26 @@
 package com.mydev.java;
 
+enum Lifecycle {
+	START (1),
+	INPROGRESS(2),
+	DONE(3);
+	final int stage;
+	private Lifecycle (int stage) {
+		this.stage = stage;
+	}
+	 
+	public int state () {
+		return stage;
+	}
+}
+
 public class Enums {
 	public static void main(String[] args) {
-
+		Lifecycle stage = Lifecycle.INPROGRESS;
+		System.out.println(stage.toString());
+		
+		if (Lifecycle.INPROGRESS.state() == 2)
+			System.out.println("true");
 	}
 
-	public static void display(String... names) {
-
-	}
 }
